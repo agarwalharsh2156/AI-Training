a04f2a5f5ca18f725a6d3273ce1d29ad2f038ca5
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-04-01 18:16:48 +0530

    Understood the reason why @property to be used and what it facilitates instead of custom getters and setters.

--- Week1/encapsulation.py
+++ Week1/encapsulation.py
@@ -1,5 +1,7 @@
+# Class example using the property decorator.
 class BankAccount:
     bank_name = "Harsh Global Bank"         # Class variable
+
     def __init__(self, account_number, name, amount):
         # when the variables are represented with the self parameter they become instance variables.
         self._account_number = account_number   # Protected naming standard in python. Will be still accessible but not encouraged.
@@ -8,7 +10,7 @@ class BankAccount:
 
     # @property asks python to treat the python it's decorating as a standard attribute
     @property
-    def balance(self, amount = 0):
+    def balance(self):
         return self.__balance
     
     @balance.setter
@@ -25,7 +27,22 @@ class BankAccount:
         self.balance = self.balance - amount
 
 
+class BankAcc:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def set_balance(self, amount):
+        self.__balance += amount if amount > 0 else 0
+        return f"Deposited {amount}"
+
+    def get_balance(self):
+        return self.__balance
+
+
 if __name__ == "__main__":
+
+    # object creation with class object where getters and setters are created using @property.
     my_account = BankAccount(5575, "Prakash", 1000)
     print(my_account.balance)       
 
@@ -36,3 +53,22 @@ if __name__ == "__main__":
     # calling the withdrawal function
     my_account.withdraw(1000)
     print("After Withdrawal: ", my_account.balance)
+
+    # Using name mangling to return the variable with private naming standard.
+    print(my_account._BankAccount__balance)
+
+
+
+    # object creation of a class without @property decorator creating the getters and setters.
+    account = BankAcc("Aditi", 100000)
+    # use name mangling standard to view the variable.
+    print("Initial Balance: ", account._BankAcc__balance)
+
+    print("Using setter for balance updation: ", account.set_balance(1000))
+    print("Balance after deposit using getter for balance: ", account.get_balance())
+
+    # doesn't allow you to access the variable directly
+    # print(account.__balance)
+
+    # use name mangling standard to view the variable.
+    print("Using name mangling for retrieving the balance value", account._BankAcc__balance)
--- Week1/inheritance.py
+++ Week1/inheritance.py
@@ -1,4 +1,4 @@
-from Week1.encapsulation import BankAccount
+from Week1.encapsulation import BankAccount, BankAcc
 
 class SavingsAccount(BankAccount):
     def __init__(self, account_number, name, amount):
@@ -8,20 +8,39 @@ class SavingsAccount(BankAccount):
     # def show(self):
     #     return self._BankAccount__balance
 
+    # method overriding in python for deposit function
+    def deposit(self, amount):
+        self.balance += amount + 10
+        
+    # accessing a private variable from the parent class.
     def show(self):
         return self.balance
 
+
+# You cannot use the private variable without name mangling format if you don't use @property decorator
+# A very important reason to use getters and setters with the help of @property decorator instead of creating your own.
+class SavingsAcc(BankAcc):
+    def __init__(self, name, balance):
+        super().__init__(name = name, balance= balance)
+
+    def show(self):
+        return self._BankAcc__balance
+
 if __name__ == "__main__":
-    # creating an object of the Savings Account.
-    sav = SavingsAccount(5575, "Mohit", 100)
-    print(f"SavingsAccount-{sav.name}-Balance: ", sav.show())
+    # # creating an object of the Savings Account.
+    # sav = SavingsAccount(5575, "Mohit", 100)
+    # print(f"SavingsAccount-{sav.name}-Balance: ", sav.show())
 
-    # child class accessing methods from parent class.
-    # and using the mangled name for accessing the 
-    # private attribute from parent class [Not a good practice at all].
-    sav.deposit(2000)
-    print("After deposit of 2000: ", sav.show())
+    # # child class accessing methods from parent class.
+    # # and using the mangled name for accessing the 
+    # # private attribute from parent class [Not a good practice at all].
+    # sav.deposit(2000)
+    # print("After deposit of 2000: ", sav.show())
 
-    # amount available in the bank after withdrawal
-    sav.withdraw(200)
-    print("Pending amount after withdrawal of 200: ", sav.show())
+    # # amount available in the bank after withdrawal
+    # sav.withdraw(200)
+    # print("Pending amount after withdrawal of 200: ", sav.show())
+
+    #object creation for the class with no @property decorator
+    account = SavingsAcc("Aditi", 20000)
+    print(account.show())
