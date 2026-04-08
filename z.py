e714af8d92b55eca04b5e523dbe076a5074df801
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-04-08 15:10:22 +0530

    Pandas Practice commenced

--- z.py
+++ z.py
@@ -8,3 +8,12 @@ print(dictionary)
 ## OUTPUT:
 # {'a': 2}
 
+import numpy as np
+l = [1,2,3,4]
+
+a = np.array(l)
+mask = [True, False, True, False]
+print(a[mask])
+print(a[a == 1])
+print(a > 8)
+
