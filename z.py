ce1a31fdc451c0fde5ed1ef7f92d97f80ccf8bb6
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-04-16 23:18:40 +0530

    Implemented all the algorithms related to Linear and Logistic Regression

--- z.py
+++ z.py
@@ -1,19 +1,49 @@
-dictionary = {
-    "a" : 1,
-    "a" : 2
-}
+# dictionary = {
+#     "a" : 1,
+#     "a" : 2
+# }
 
-print(dictionary)
+# print(dictionary)
 
-## OUTPUT:
-# {'a': 2}
+# ## OUTPUT:
+# # {'a': 2}
 
+# import numpy as np
+# l = [1,2,3,4]
+
+# a = np.array(l)
+# mask = [True, False, True, False]
+# print(a[mask])
+# print(a[a == 1])
+# print(a > 8)
 import numpy as np
-l = [1,2,3,4]
+a = np.array([
+    [1, 2, 4],
+    [1, 2, 4],
+    [1, 2, 4],
+    [1, 2, 4],
+    [1, 2, 4],
+    [1, 2, 4],
+    [1, 2, 4],
+])
 
-a = np.array(l)
-mask = [True, False, True, False]
-print(a[mask])
-print(a[a == 1])
-print(a > 8)
+w = np.array([
+    [2],
+    [2],
+    [2],
+    
+])
+
+e = np.ones(shape=(3,1))
+print(w * e)
+
+
+b = np.ones(shape=(7,1))
+print(b)
+r = a@w
+print(a@w)
+print(a.shape)
+print(w.shape)
+print(r+b)
+
 
