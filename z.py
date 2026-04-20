e3916c5aa1cace71c4829751269348c693f14f51
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-04-20 23:57:02 +0530

    Started FastAPI implementation

--- z.py
+++ z.py
@@ -16,34 +16,54 @@
 # print(a[mask])
 # print(a[a == 1])
 # print(a > 8)
-import numpy as np
-a = np.array([
-    [1, 2, 4],
-    [1, 2, 4],
-    [1, 2, 4],
-    [1, 2, 4],
-    [1, 2, 4],
-    [1, 2, 4],
-    [1, 2, 4],
-])
+# import numpy as np
+# a = np.array([
+#     [1, 2, 4],
+#     [1, 2, 4],
+#     [1, 2, 4],
+#     [1, 2, 4],
+#     [1, 2, 4],
+#     [1, 2, 4],
+#     [1, 2, 4],
+# ])
 
-w = np.array([
-    [2],
-    [2],
-    [2],
+# w = np.array([
+#     [2],
+#     [2],
+#     [2],
     
-])
+# ])
 
-e = np.ones(shape=(3,1))
-print(w * e)
+# e = np.ones(shape=(3,1))
+# print(w * e)
 
 
-b = np.ones(shape=(7,1))
-print(b)
-r = a@w
-print(a@w)
-print(a.shape)
-print(w.shape)
-print(r+b)
+# b = np.ones(shape=(7,1))
+# print(b)
+# r = a@w
+# print(a@w)
+# print(a.shape)
+# print(w.shape)
+# print(r+b)
 
+
+
+
+students = {
+    1: {
+        "name" : "Harsh",
+        "college": "PICA"
+    },
+    2: {
+        "name": "Krisha",
+        "college": "PICA"
+    },
+    3: {
+        "name": "Mahi",
+        "college": "Jai Hind"
+    }
+}
+
+for key in students[1].keys():
+    print(students[1][key])
 
