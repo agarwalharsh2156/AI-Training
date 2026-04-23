ccba57432524a32c35cb8772c0cf7dbea10b7717
Harsh Agarwal <agarwalharsh2156@gmail.com>
2026-04-23 22:42:48 +0530

    Implemented a middleware.

--- Week3/SqlAlchemyPractice.py
+++ Week3/SqlAlchemyPractice.py
@@ -25,3 +25,20 @@ def get_user_token(token: str, db = Depends(get_db)):
 def get_item(db = Depends(get_db), user = Depends(get_user_token)):
     return {"user": user["name"], "items": db["items"]}
 
+
+# Middlewares
+
+import time
+from fastapi import Request
+
+@app.middleware("http")
+async def add_timing(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    duration = time.time() - start
+    response.headers["X-Process-Time"] = str(duration)
+    return response
+
+@app.get("/middleware-check")
+def middleware_check():
+    return {"message": "Middleware must return time duration data."}
